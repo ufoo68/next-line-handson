3a0a9b1ecef634086555a064f043f14504b6a2eb
ufoo68 <mitarou.yuutarou@gmail.com>
2023-09-12 05:43:07 +0000

    remove comment

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -2,20 +2,18 @@
 
 import { signIn, signOut } from "next-auth/react";
 
-// ログインボタン
 export const LoginButton = () => {
     return (
         <button onClick={() => signIn()}>
-            Sign in
+           ログイン
         </button>
     );
 };
 
-// ログアウトボタン
 export const LogoutButton = () => {
     return (
         <button onClick={() => signOut()}>
-            Sign Out
+            ログアウト
         </button>
     );
 };
