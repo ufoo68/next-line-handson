5404e4e4eb39df1bb6d5aade223282a7f23f4f09
ufoo68 <mitarou.yuutarou@gmail.com>
2023-09-12 05:41:48 +0000

    remove log

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { authOptions } from './options';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  console.info(session);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       {session ? <LogoutButton/> : <LoginButton/>}
