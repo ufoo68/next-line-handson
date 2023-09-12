bb25518fe9e4ca90691fd2aba17c6ea4e993199e
ufoo68 <mitarou.yuutarou@gmail.com>
2023-09-12 05:30:06 +0000

    add log

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { authOptions } from './api/auth/[...nextauth]/route';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  console.info(session);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       {session ? <LogoutButton/> : <LoginButton/>}
