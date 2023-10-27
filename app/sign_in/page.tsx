72b10da92476ee326c512de5280c9419ba5747c1
ufoo68 <mitarou.yuutarou@gmail.com>
2023-10-27 04:11:12 +0000

    fix indent

--- app/sign_in/page.tsx
+++ app/sign_in/page.tsx
@@ -3,7 +3,7 @@ import { LoginButton, LogoutButton } from '../components/buttons'
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-       <LoginButton/>
+      <LoginButton/>
     </main>
   )
 }
