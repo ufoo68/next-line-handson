774a4e6c117be44eac8c74d34de8b436b67109bd
ufoo68 <mitarou.yuutarou@gmail.com>
2023-10-27 04:21:01 +0000

    fix style

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -4,7 +4,10 @@ import { signIn, signOut } from "next-auth/react";
 
 export const LoginButton = () => {
   return (
-    <button className="btn btn-blue" onClick={() => signIn('line', { callbackUrl: '/' })}>
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={() => signIn('line', { callbackUrl: '/' })}
+    >
       ログイン
     </button>
   );
@@ -12,7 +15,10 @@ export const LoginButton = () => {
 
 export const LogoutButton = () => {
   return (
-    <button className="btn btn-blue" onClick={() => signOut()}>
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={() => signOut()}
+    >
       ログアウト
     </button>
   );
