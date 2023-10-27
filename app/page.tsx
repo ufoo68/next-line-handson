1432895ad43bc7d8e28550e2dfd6c3416ecfa1d2
ufoo68 <mitarou.yuutarou@gmail.com>
2023-10-27 04:06:15 +0000

    仕様変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { getServerSession } from 'next-auth';
 import { LoginButton, LogoutButton } from './components/buttons'
 import { authOptions } from './options';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect('/sign_in')
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      {session ? <LogoutButton/> : <LoginButton/>}
+      <div>Welcome: {session?.user?.name}</div>
+      <LogoutButton/>
     </main>
   )
 }
